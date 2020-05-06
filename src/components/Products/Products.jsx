719b1b954f5bb32af8c6c7dcc17e5641ee89855f
Cristian David Pinto Penaloza <cristiandavid.pinto@globant.com>
2020-05-06 15:44:14 -0500

    Added Products List

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 function Products() {
   return (
-    <div>
-      <h2>From Products</h2>
-    </div>
+    <Fragment>
+      <h2 className="text-center my-5">Products List</h2>
+
+      {/* {error ? (
+        <p className="font-weight-bold alert alert-danger text-center mt-4">
+          Hubo un error
+        </p>
+      ) : null}
+
+      {cargando ? <p className="text-center">Cargando....</p> : null} */}
+
+      <table className="table table-striped">
+        <thead className="bg-primary table-dark">
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Price</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        {/* <tbody>
+          {productos.length === 0
+            ? 'No hay productos'
+            : productos.map((producto) => (
+                <Producto key={producto.id} producto={producto} />
+              ))}
+        </tbody> */}
+      </table>
+    </Fragment>
   );
 }
 
